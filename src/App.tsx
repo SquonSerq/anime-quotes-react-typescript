53923f607525c3b62efc9ad4ca7202cd7b1e6695
SquonSerq <squonserq@bk.ru>
2022-12-01 04:51:41 +0600

    [~] add initial fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import * as React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import IQuote from './types/types';
+
 const App = () => {
-  return ( <div>WOrk!</div> );
+  const [quote, setQuote] = useState<IQuote>()
+
+	async function fetchQuote() {
+		try{
+			const response = await axios.get('https://animechan.vercel.app/api/random')
+      setQuote(response.data)
+		} catch(e){
+			alert(e)
+		}
+	}
+
+	useEffect(() => {
+    fetchQuote()
+	}, [])
+
+  return ( 
+    <div>
+      <p>{quote?.anime}</p>
+      <p>{quote?.character}</p>
+      <p>{quote?.quote}</p>
+    </div>
+  );
 }
- 
+
 export default App;
---
+++ src/types/types.tsx
@@ -0,0 +1,5 @@
+export default interface IQuote {
+	anime: string,
+	character: string,
+	quote: string
+}
